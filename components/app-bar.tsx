fbdb23ca27645a93f8ed19b7a8f55f93ad62b5e3
stkevintan <stkevintan@zju.edu.cn>
2020-07-19 19:36:18 +0800

    fix; add github link

--- components/app-bar.tsx
+++ components/app-bar.tsx
@@ -164,7 +164,7 @@ export default function SearchAppBar({ toggleDrawer, open }: Props) {
                         />
                     </div>
                     <div>
-                        <IconButton color="inherit">
+                        <IconButton href="https://github.com/CloudAurora" color="inherit">
                             <GitHubIcon />
                         </IconButton>
                     </div>
