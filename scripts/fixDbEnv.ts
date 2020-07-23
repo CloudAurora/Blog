d0a79ac6554ca4dd29b7897e91d66b0630e4485c
stkevintan <stkevintan@zju.edu.cn>
2020-07-23 10:37:16 +0800

    feat: move db to public

--- scripts/fixDbEnv.ts
+++ scripts/fixDbEnv.ts
@@ -1,12 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-const relativePath = './prisma/dev.db';
+const relativePath = './public/dev.db';
 if (!fs.existsSync(relativePath)) {
     throw new Error(`Cannot find sqlite database at: ${relativePath}`);
 }
 
-const absPath = path.resolve(relativePath)
+const absPath = '/dev.db' 
 
 const text = fs.readFileSync('./prisma/schema.prisma', {encoding: 'utf-8'});
 fs.writeFileSync('./prisma/schema.prisma', text.replace(/^(\s*url\s*=\s*)\S*$/m, `$1"file:${absPath}"`));
