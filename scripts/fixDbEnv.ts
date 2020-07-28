56c8fecaa2a48f510a7305c465f714f92b6d335a
stkevintan <stkevintan@zju.edu.cn>
2020-07-28 12:57:18 +0800

    fix: path

--- scripts/fixDbEnv.ts
+++ scripts/fixDbEnv.ts
@@ -1,14 +1,17 @@
 import fs from 'fs';
+import path from 'path';
 
-const relativePath = './prisma/dev.db';
-if (!fs.existsSync(relativePath)) {
-    throw new Error(`Cannot find sqlite database at: ${relativePath}`);
+const sourcePath = './prisma/dev.db';
+if (!fs.existsSync(sourcePath)) {
+    throw new Error(`Cannot find sqlite database at: ${sourcePath}`);
 }
 
-const absPath = './node_modules/dev.db' 
+const targetPath = './node_modules/dev.db' 
+
+fs.copyFileSync(sourcePath, targetPath);
 
 const text = fs.readFileSync('./prisma/schema.prisma', {encoding: 'utf-8'});
-fs.writeFileSync('./prisma/schema.prisma', text.replace(/^(\s*url\s*=\s*)\S*$/m, `$1"file:${absPath}"`));
+fs.writeFileSync('./prisma/schema.prisma', text.replace(/^(\s*url\s*=\s*)\S*$/m, `$1"file:${path.resolve('../', targetPath)}"`));
 // fs.writeFileSync('./prisma/.env', `SQLITE_DB_URL="file:${absPath}"`, { encoding: 'utf-8' });
 
-console.log("set sqlite db path env to ", absPath);
+console.log("set sqlite db path env to ", targetPath);
